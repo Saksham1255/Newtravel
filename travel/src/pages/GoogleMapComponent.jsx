d6f76f9e3e9ffb1259ef1eee4410a830bf69e3b2
Saksham <saksham29321@gmaiil.com>
2025-06-09 00:08:11 +0530

    Your commit message

---
+++ travel/src/pages/GoogleMapComponent.jsx
@@ -0,0 +1,33 @@
+// src/pages/MapPage.js
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const MapPage = () => {
+  const position = [20.5937, 78.9629]; // Center of India
+
+  return (
+    <MapContainer center={position} zoom={5} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          This is India!
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapPage;
